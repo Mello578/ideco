d8777d7abd60099959636671eb0bbc72e6309a38
mel <mello.shlyapa@gmail.com>
2018-05-03 00:53:34 +0500

    сортировка

--- server/utils/sortingByColumns.js
+++ server/utils/sortingByColumns.js
@@ -1,21 +1,52 @@
+let flag = false;
+
 function sortingByColumns(column, data) {
-  let allData = data;
-  let sortedData;
+  flag = !flag;
+
   switch (column) {
     case 0:
-      sortedData = allData.sort((one, two) => one.airlines.name.localeCompare(two.airlines.name));
-      break;
+      return data.sort((a, b) => flag
+        ? a.airlines.name.localeCompare(b.airlines.name)
+        : b.airlines.name.localeCompare(a.airlines.name)
+      );
     case 1:
-      sortedData = allData.sort((one, two) => one.airlines.flight.localeCompare(two.airlines.flight));
-      break;
+      return data.sort((a, b) => flag
+        ? a.airlines.flight.localeCompare(b.airlines.flight)
+        : b.airlines.flight.localeCompare(a.airlines.flight)
+      );
     case 2:
-      sortedData = allData.sort((one, two) => one.aircraft.typeJet.localeCompare(two.aircraft.typeJet));
-      break;
+      return data.sort((a, b) => flag
+        ? a.aircraft.typeJet.localeCompare(b.aircraft.typeJet)
+        : b.aircraft.typeJet.localeCompare(a.aircraft.typeJet)
+      );
     case 3:
-      sortedData = allData.sort((one, two) => one.departureCity.city.localeCompare(two.departureCity.city));
-      break;
+      return data.sort((a, b) => flag
+        ? a.departureCity.city.localeCompare(b.departureCity.city)
+        : b.departureCity.city.localeCompare(a.departureCity.city)
+      );
+    case 4:
+      return data.sort((a, b) => flag
+        ? a.allDataTime.timeDepart.getTime() > b.allDataTime.timeDepart.getTime() ? 1 : -1
+        : b.allDataTime.timeDepart.getTime() > a.allDataTime.timeDepart.getTime() ? 1 : -1
+      );
+    case 5:
+      return data.sort((a, b) => flag
+        ? a.arrivalCity.city.localeCompare(b.arrivalCity.city)
+        : b.arrivalCity.city.localeCompare(a.arrivalCity.city)
+      );
+    case 6:
+      return data.sort((a, b) => flag
+        ? a.allDataTime.timeArrival.getTime() > b.allDataTime.timeArrival.getTime() ? 1 : -1
+        : b.allDataTime.timeArrival.getTime() > a.allDataTime.timeArrival.getTime() ? 1 : -1
+      );
+    case 7:
+      return data.sort((a, b) => flag
+        ? a.allDataTime.expectedTime.getTime() > b.allDataTime.expectedTime.getTime() ? 1 : -1
+        : b.allDataTime.expectedTime.getTime() > a.allDataTime.expectedTime.getTime() ? 1 : -1
+      );
+    default:
+      return data
   }
-  return sortedData;
 }
 
 module.exports = sortingByColumns;
